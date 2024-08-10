29ae71138f6b4f231cc6cddde414bb3dbb1d1d99
Pradip Chapagain <pradipcpgn@gmail.com>
2024-08-10 18:17:50 +0545

    feat:vercel anaytics

--- src/main.tsx
+++ src/main.tsx
@@ -6,12 +6,14 @@ import { store } from "../redux/store.ts";
 import { fetchSessionData } from "../redux/slices/SessionSlice.ts";
 import ReduxProvider from "../redux/ReduxProvider.tsx";
 import WsProvider from "../utils/WsProvider.tsx";
+import { Analytics } from "@vercel/analytics/react";
 
 store.dispatch(fetchSessionData());
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ReduxProvider>
       <WsProvider>
+        <Analytics />
         <App />
       </WsProvider>
     </ReduxProvider>
